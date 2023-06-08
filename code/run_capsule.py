7a2bd919ef9ccdefffd415671ad065b00d6c9249
Alessio Buccino <alessio.buccino@alleninstitute.org>
2023-06-08 14:05:48 +0000

    Edited run_capsule.py, Dockerfile

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -16,6 +16,7 @@ from datetime import datetime, timedelta
 # SPIKEINTERFACE
 import spikeinterface as si
 import spikeinterface.postprocessing as spost
+import spikeinterface.qualitymetrics as sqm
 import spikeinterface.curation as sc
 
 from spikeinterface.core.core_tools import check_json
@@ -33,6 +34,58 @@ sparsity_params=dict(
     method="radius",
     radius_um=100
 )
+
+qm_params = {
+    'presence_ratio': {'bin_duration_s': 60},
+    'snr':  {
+        'peak_sign': 'neg',
+        'peak_mode': 'extremum',
+        'random_chunk_kwargs_dict': None
+    },
+    'isi_violation': {
+        'isi_threshold_ms': 1.5, 'min_isi_ms': 0
+    },
+    'rp_violation': {
+        'refractory_period_ms': 1, 'censored_period_ms': 0.0
+    },
+    'sliding_rp_violation': {
+        'bin_size_ms': 0.25,
+        'window_size_s': 1,
+        'exclude_ref_period_below_ms': 0.5,
+        'max_ref_period_ms': 10,
+        'contamination_values': None
+    },
+    'amplitude_cutoff': {
+        'peak_sign': 'neg',
+        'num_histogram_bins': 100,
+        'histogram_smoothing_value': 3,
+        'amplitudes_bins_min_ratio': 5
+    },
+    'amplitude_median': {
+        'peak_sign': 'neg'
+    },
+    'nearest_neighbor': {
+        'max_spikes': 10000, 'min_spikes': 10, 'n_neighbors': 4
+    },
+    'nn_isolation': {
+        'max_spikes': 10000,
+        'min_spikes': 10,
+        'n_neighbors': 4,
+        'n_components': 10,
+        'radius_um': 100
+    },
+    'nn_noise_overlap': {
+        'max_spikes': 10000,
+        'min_spikes': 10,
+        'n_neighbors': 4,
+        'n_components': 10,
+        'radius_um': 100
+    }
+}
+qm_metric_names = ['num_spikes', 'firing_rate', 'presence_ratio', 'snr',
+                   'isi_violation', 'rp_violation', 'sliding_rp_violation',
+                   'amplitude_cutoff', 'drift', 'isolation_distance',
+                   'l_ratio', 'd_prime']
 
 postprocessing_params = dict(
     duplicate_threshold=0.9,
@@ -64,6 +117,7 @@ postprocessing_params = dict(
     principal_components=dict(n_components=5,
                               mode='by_channel_local',
                               whiten=True),
+    quality_metrics=dict(qm_params=qm_params, metric_names=qm_metric_names, n_jobs=1),
 )
 
 job_kwargs = {
@@ -86,10 +140,9 @@ if __name__ == "__main__":
     
     si.set_global_job_kwargs(**job_kwargs)
 
-    ####### SPIKESORTING ########
-    print("\n\nPOSTPROCESSING")
+    ####### POSTPROCESSING ########
+    print("\n\POSTPROCESSING")
     postprocessing_notes = ""
-    sorting_params = None
 
     datetime_start_postprocessing = datetime.now()
     t_postprocessing_start = time.perf_counter()
@@ -139,6 +192,8 @@ if __name__ == "__main__":
         del we_raw
 
         wf_sparse_folder = results_folder / "postprocessed" / recording_name
+        # this is a trick to make the postprocessed folder "self-contained
+        sorting = sorting.save(folder=results_folder / "postprocessed" / f"{recording_name}_sorting")
 
         # now extract waveforms on de-duplicated units
         print(f"\tSaving sparse de-duplicated waveform extractor folder")
@@ -161,8 +216,8 @@ if __name__ == "__main__":
         tm = spost.compute_template_metrics(we, **postprocessing_params["template_metrics"])
         print("\tComputing PCA")
         pc = spost.compute_principal_components(we, **postprocessing_params["principal_components"])
-
-    
+        print("\tComputing quality metrics")
+        qm = sqm.compute_quality_metrics(we, **postprocessing_params["quality_metrics"])
 
     t_postprocessing_end = time.perf_counter()
     elapsed_time_postprocessing = np.round(t_postprocessing_end - t_postprocessing_start, 2)
