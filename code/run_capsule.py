5a484abad1d6752a07d1451e267ec443723120af
Alessio Buccino <alessio.buccino@alleninstitute.org>
2023-10-18 11:27:09 +0000

    Cast n_jobs as int

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -122,7 +122,8 @@ postprocessing_params = dict(
 
 
 n_jobs_co = os.getenv('CO_CPUS')
-n_jobs = n_jobs_co if n_jobs_co is not None else -1
+print(n_jobs_co)
+n_jobs = int(n_jobs_co) if n_jobs_co is not None else -1
 
 job_kwargs = {
     'n_jobs': n_jobs,
