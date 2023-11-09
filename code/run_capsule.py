dcbf026b2d3862b2fd2a2cb16ebc6060890718a0
Alessio Buccino <alessio.buccino@alleninstitute.org>
2023-11-09 12:41:26 +0000

    use 80% of the n_jobs

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -127,7 +127,7 @@ postprocessing_params = dict(
 
 
 n_jobs_co = os.getenv("CO_CPUS")
-n_jobs = int(n_jobs_co) if n_jobs_co is not None else -1
+n_jobs = int(0.8 * n_jobs_co) if n_jobs_co is not None else 0.8
 
 job_kwargs = {"n_jobs": n_jobs, "chunk_duration": "1s", "progress_bar": False}
 
